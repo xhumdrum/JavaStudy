6890a735fd9a9591792323e5c0186aa400534ca5
yhumdrum <xiang_xia@126.com>
2022-04-27 22:47:02 +0800

    2022.04.27-02 yhumdrum

--- JavaBasic/src/com/basic/helloworld/HelloWorld.java
+++ JavaBasic/src/com/basic/helloworld/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
         System.out.println("Hello World!");
         System.out.println("Git协同开发测试....");
         System.out.println("协同开发测试22222");
-
+        System.out.println("test");
     }
 }
