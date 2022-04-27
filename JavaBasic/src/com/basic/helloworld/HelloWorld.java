fbe2554ce6d4ae4cbe9ad5e7532311d350641328
yhumdrum <xiang_xia@126.com>
2022-04-27 17:59:53 +0800

    2022.04.27-01 yhumdrum

--- JavaBasic/src/com/basic/helloworld/HelloWorld.java
+++ JavaBasic/src/com/basic/helloworld/HelloWorld.java
@@ -3,5 +3,8 @@ package com.basic.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Git协同开发测试....");
+
+
     }
 }
